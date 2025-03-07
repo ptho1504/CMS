019a4f369b95b6e15121f2eaa890ad0b82a30d0f
ptho1504 <phuoctho150420@gmail.com>
2025-03-07 16:49:56 +0700

    add grpc - conflict between spring boot 3.4.2 vs 3.4.3

--- CMS_BE/order-service/src/main/java/com/ptho1504/microservice/order_service/order/controller/OrderController.java
+++ CMS_BE/order-service/src/main/java/com/ptho1504/microservice/order_service/order/controller/OrderController.java
@@ -1,8 +1,10 @@
 package com.ptho1504.microservice.order_service.order.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/orders")
 public class OrderController {
     
 }
--- CMS_BE/order-service/src/main/java/com/ptho1504/microservice/order_service/order/controller/OrderItemController.java
+++ CMS_BE/order-service/src/main/java/com/ptho1504/microservice/order_service/order/controller/OrderItemController.java
@@ -1,8 +1,11 @@
 package com.ptho1504.microservice.order_service.order.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
+@RequestMapping("/api/v1/order-items")
 public class OrderItemController {
 
 }
