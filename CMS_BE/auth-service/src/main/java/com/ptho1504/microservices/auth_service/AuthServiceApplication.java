57a229a047df17e77ba2bf28386e57e9a35bb969
ptho1504 <phuoctho150420@gmail.com>
2025-03-28 16:28:19 +0700

    add docker compose

--- CMS_BE/auth-service/src/main/java/com/ptho1504/microservices/auth_service/AuthServiceApplication.java
+++ CMS_BE/auth-service/src/main/java/com/ptho1504/microservices/auth_service/AuthServiceApplication.java
@@ -2,10 +2,12 @@ package com.ptho1504.microservices.auth_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import net.devh.boot.grpc.server.autoconfigure.GrpcServerSecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { GrpcServerSecurityAutoConfiguration.class })
+@EnableDiscoveryClient
 public class AuthServiceApplication {
 
 	public static void main(String[] args) {
